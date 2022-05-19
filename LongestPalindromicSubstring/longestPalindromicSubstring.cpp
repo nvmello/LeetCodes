b555ee603e15c141b991961307f6afdf79c21232
Nicholas Morello <nickvmorello@gmail.com>
2022-05-19 14:06:31 -0700

    Initial commit, cpp setup, plus readme

---
+++ LongestPalindromicSubstring/longestPalindromicSubstring.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+    }
+};
