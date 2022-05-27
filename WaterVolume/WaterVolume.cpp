e916d61c2b7a7db0133e73bba3623bde85bfd02f
Nicholas Morello <nickvmorello@gmail.com>
2022-05-26 22:07:43 -0700

    Reworked algorithm to imporve time complexity to O(n) using values to compare either side of the vector and move one at a time to meet in the middle.

--- WaterVolume/WaterVolume.cpp
+++ WaterVolume/WaterVolume.cpp
@@ -5,20 +5,35 @@ using namespace std;
 
 class Solution {
 public:
+    /**
+     * More efficient rework of previous algorithm, O(n) time complexity because starting at either end of the vector
+     * and meeting in the middle allows us to traverse the vector only once
+     */
     int maxArea(vector<int>& height) {
-        
-        vector<int> volume;     //Vector to hold the volumes for each pair of heights
-        for(int i = 0; i < height.size(); i++){ 
-            for(int j = i+1; j < height.size(); j++){   //only compare each height to every height in front of it so not to measure things twice
-                
-                //take the min of each height pair and mult by distance between them
-                //push the product into the volumes vector
-                volume.push_back(min(height[i], height[j])*(j-i));  
-                
+        int temp = 0;           //temp var for comparing each interations volume with the current max volume
+        int maxVol = 0;         //max volume to be returned
+        int l = 0;              //value beginning on the left hand side of the vector
+        int r= height.size()-1; //value beginning on the right hand side of the vector
+
+        while (l < r)   //while the left point is smaller than the right point
+        {
+            // cout<<height[l] << " : " << height[r] << endl;
+            temp = min(height[l], height[r]) * (r-l);   //set the temp value to the most recently calculated volume
+            if (temp > maxVol)                      //compare to the current max value
+            {
+                maxVol = temp;                      // reset max value if a new max is found
             }
+            // cout<<" l = " << l << " r = " << r << endl;
+            // cout<<temp<<endl;
+            if (height[l] <=  height[r])    //if the left side is smaller than the right
+            {
+                l++;                            // move the left side right one place
+            } else{
+                r--;                            //otherwise move the right side left one place
+            } 
         }
-        // cout<<*max_element(volume.begin(), volume.end())<<endl; 
-        return *max_element(volume.begin(), volume.end()); //return the max value from volumes vector
+        
+       return maxVol;
     }
 };
 
